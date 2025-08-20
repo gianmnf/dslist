64ca61f0ba41086b97c6b50eea4c0491eb884e3f
Gian Michel <gianmnf@gmail.com>
2025-08-20 15:19:33 -0300

    Deploy ready

--- src/main/java/com/devsuperior/dslist/repositories/GameRepository.java
+++ src/main/java/com/devsuperior/dslist/repositories/GameRepository.java
@@ -1,9 +1,21 @@
 package com.devsuperior.dslist.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.devsuperior.dslist.entities.Game;
+import com.devsuperior.dslist.projections.GameMinProjection;
 
 public interface GameRepository extends JpaRepository<Game, Long> {
-
+	@Query(nativeQuery = true, value = """
+			SELECT g.id, g.title, g.game_year AS "year", g.img_url AS imgUrl,
+			g.short_description as shortDescription, b.position
+			FROM tb_game g
+			INNER JOIN tb_belonging b on g.id = b.game_id
+			WHERE b.list_id = :listId
+			ORDER BY b.position
+			""")
+	List<GameMinProjection> searchByList(Long listId);
 }
